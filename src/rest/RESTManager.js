9eaa97efddaccffa927f5a572ec52a43fd3fcae7
cola cat <86305856+swagcat7889@users.noreply.github.com>
2021-08-05 16:15:42 +0200

    Update RESTManager.js

--- src/rest/RESTManager.js
+++ src/rest/RESTManager.js
@@ -26,8 +26,11 @@ class RESTManager {
   }
 
   getAuth() {
-    const token = this.client.token || this.client.accessToken;
-    if (token) return `${this.tokenPrefix} ${token}`;
+    if (this.client.token && this.client.user && this.client.user.bot) {
+      return `Bot ${this.client.token}`;
+    } else if (this.client.token) {
+      return this.client.token;
+    }
     throw new Error('TOKEN_MISSING');
   }
 
